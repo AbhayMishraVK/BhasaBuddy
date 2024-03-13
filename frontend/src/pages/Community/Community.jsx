603fb6e97827cd63a045dec525ac77c190563102
AryanKelwa <126327700+AryanKelwa@users.noreply.github.com>
2024-03-13 22:22:25 +0530

    commnity jsx update

--- frontend/src/pages/Community/Community.jsx
+++ frontend/src/pages/Community/Community.jsx
@@ -1,62 +1,54 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-// import '../assets/style/community.css';
-
+import axios from 'axios';
+import styles from './community.module.css'; // Import CSS module
+import profile from './images/profile.jpg';
+import communityImage from './images/community.png';
 
 function CommunityPage() {
-    const [posts, setPosts] = useState([]);
-    const [newPostContent, setNewPostContent] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [disabledButtons, setDisabledButtons] = useState([]);
-
-    useEffect(() => {
-      fetchPosts();
-    }, []);
+  const [posts, setPosts] = useState([]);
+  const [newPostContent, setNewPostContent] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [disabledButtons, setDisabledButtons] = useState([]);
 
 
-    const fetchPosts = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get("http://127.0.0.1:5000/community/posts");
-        console.log(response)
-        if (response.status === 200) {
-          setPosts(response.data.posts);
-        } else {
-          console.error("Error fetching posts. Status:", response.status);
-        }
-      } catch (error) {
-        console.error("Error fetching posts:", error);
-      } finally {
-        setLoading(false);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/community/posts");
+      if (response.status === 200) {
+        setPosts(response.data.posts);
+      } else {
+        console.error("Error fetching posts. Status:", response.status);
       }
-    };
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  const handleNewPostSubmit = async (e) => {
+    e.preventDefault();
+    if (!newPostContent.trim()) return;
 
-    const handleNewPostSubmit = async (e) => {
-      e.preventDefault();
-      if (!newPostContent.trim()) return;
+    const email = localStorage.getItem('email');
 
-      // Retrieve email from local storage
-      const email = localStorage.getItem('email');
-      console.log(email)
-
-      try {
-        // Make POST request with email and new post content
-        const response = await axios.post("http://127.0.0.1:5000/community/post", { email: email, content: newPostContent });
-        
-        // Check if the status code is 201
-        if (response.status === 201) {
-          // Clear the new post content after successful submission
-          setNewPostContent("");
-          // Refresh posts after new post submission
-          fetchPosts();
-        } else {
-          console.error("Error submitting new post. Unexpected status:", response.status);
-        }
-      } catch (error) {
-        console.error("Error submitting new post:", error);
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/community/post", { email: email, content: newPostContent });
+      if (response.status === 201) {
+        setNewPostContent("");
+        fetchPosts();
+      } else {
+        console.error("Error submitting new post. Unexpected status:", response.status);
       }
-    };
+    } catch (error) {
+      console.error("Error submitting new post:", error);
+    }
+  };
 
   const handleLike = async (postId, index) => {
     try {
@@ -93,43 +85,56 @@ function CommunityPage() {
   };
 
   return (
-    <div className="community-page">
-      <h1>Community Page</h1>
+    <div className={styles["comm-container"]}> {/* Apply CSS module class */}
+      <div className={styles["comm-community-page"]}> {/* Apply CSS module class */}
+        <h1>Community Page</h1>
 
-      {/* New Post Form */}
-      <form className="new-post-form" onSubmit={handleNewPostSubmit}>
-        <textarea
-          value={newPostContent}
-          onChange={(e) => setNewPostContent(e.target.value)}
-          placeholder="Write your post..."
-        ></textarea>
-        <button type="submit">Submit</button>
-      </form>
+        <div className={styles["comm-top"]}> {/* Apply CSS module class */}
+          <form className={styles["comm-new-post-form"]} onSubmit={handleNewPostSubmit}> {/* Apply CSS module class */}
+            <textarea
+              value={newPostContent}
+              onChange={(e) => setNewPostContent(e.target.value)}
+              placeholder="Write your post..."
+              style={{ height: '70px' }}
+            ></textarea>
+            <button type="submit">Submit</button>
+          </form>
 
-      {/* Posts */}
-      <div className="posts">
-        <div className="posts-grid">
-          {loading ? (
-            <p>Loading posts...</p>
-          ) : (
-            posts.map((post, index) => (
-              <div className="post" key={post.id}>
-                <p>{post.post_time}</p>
-                <img src={`data:image/jpeg;base64,${post.profileImage}`} alt="Profile" />
-                <p>{post.creator_name}</p>
-                <p>{post.content}</p>
-                <button onClick={() => handleLike(post.post_id, index)} disabled={disabledButtons.includes(index)}>Like</button>
-                <span>{post.likes}</span>
-                <button onClick={() => handleDislike(post.post_id, index)} disabled={disabledButtons.includes(index)}>Dislike</button>
-                <span>{post.dislikes}</span>
-              </div>
-            ))
-          )}
+          <img style={{ height: '170px', display: 'inline' }} src={communityImage} alt="" />
+        </div>
+
+        <div className={styles["comm-posts"]}> {/* Apply CSS module class */}
+          <div className={styles["comm-posts-grid"]}> {/* Apply CSS module class */}
+            {loading ? (
+              <p>Loading posts...</p>
+            ) : (
+              posts.map((post,index) => (
+                <div className={styles["comm-post"]} key={post.id}> {/* Apply CSS module class */}
+                  <p>{post.post_time}</p>
+
+                  <div className={styles["comm-upper"]}> {/* Apply CSS module class */}
+                    <div className={styles["comm-lp"]}> {/* Apply CSS module class */}
+                      <img src={`data:image/jpeg;base64,${post.profileImage}`} alt="Profile" />
+                    </div>
+
+                    <div className={styles["comm-rp"]}> {/* Apply CSS module class */}
+                      <h2>{post.creator_name}</h2>
+                      <p>{post.content}</p>
+                    </div>
+                  </div>
+
+                  <div className={styles["comm-lower"]}> {/* Apply CSS module class */}
+                    <button onClick={() => handleLike(post.id,index) } disabled={disabledButtons.includes(index)} className={styles["comm-button"]}>Like<span style={{ marginLeft: '5px' }}>{post.likes}</span></button>
+                    <button onClick={() => handleDislike(post.id,index)} disabled={disabledButtons.includes(index)} className={styles["comm-button"]}>Dislike<span style={{ marginLeft: '5px' }}>{post.dislikes}</span></button>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
 
 export default CommunityPage;
